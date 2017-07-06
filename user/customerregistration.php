33e01d240915881010e1cf0e986e114fc8a75b7b
iterjrm <codingjrm@gmail.com>
2017-07-06 16:07:23 +0530

    user registration

--- user/customerregistration.php
+++ user/customerregistration.php
@@ -57,13 +57,24 @@
                         <div class="x_title">
                     <h2>Tell Us About Your Usage ! </h2>
 
-                       <div class="row">
-                      <div class="btn-group">
-                        <button class="btn btn-default" type="button">Left</button>
-                        <button class="btn btn-default" type="button">Middle</button>
-                        <button class="btn btn-default" type="button">Right</button>
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Gender</label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <div id="gender" class="btn-group" data-toggle="buttons">
+                            <label class="btn btn-default" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="Domestic"> &nbsp; Domestic &nbsp;
+                            </label>
+                            <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="Commercial"> Commercial
+                            </label>
+                            <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="Industrial"> Industrial
+                            </label>
+                          </div>
+                        </div>
                       </div>
 
+                      \
                       <div class="form-group">
                         
                       </div>
