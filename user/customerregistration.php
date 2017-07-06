33690959ecef1c8b17255dc53e90519f6cf63a60
sidharthsray <sidharthsray@gmail.com>
2017-07-06 17:40:46 +0530

    a

--- user/customerregistration.php
+++ user/customerregistration.php
@@ -117,8 +117,9 @@
                         <input type="text" class="form-control has-feedback-left has-feedback-right " id="inputSuccess5" placeholder="Roof Area">
                         <span class="fa fa-arrows form-control-feedback left" aria-hidden="true" style="color:lightgreen"></span>
                       </div>
-                      <span class="form-control-feedback right" aria-hidden="true" style="color:darkgrey">sq.ft</span>
+                      <span class=" form-control-feedback left" aria-hidden="true" style="color:darkgrey">sq.ft</span>
                       </div>
+                      
 
 
 
