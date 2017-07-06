3325b7bfc8dc6eb54cedbbc949de33bacac4eaea
iterjrm <codingjrm@gmail.com>
2017-07-06 22:01:27 +0530

    h

--- user/singlemail.php
+++ user/singlemail.php
@@ -45,8 +45,7 @@
                         <div class="col-md-12 col-sm-12 col-xs-12">
           
 
-                  <textarea name="descr" id="descr" style="display:none;" required="required" name="body"></textarea>
-
+              
                    </div>
                    <input type="submit" name="">
 
