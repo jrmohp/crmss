949199e3d6bd07b8c54a7716b62f5e5661fa8145
sidharthsray <sidharthsray@gmail.com>
2017-07-06 17:53:21 +0530

    s

---
+++ user/singlemail.php
@@ -0,0 +1,163 @@
+<?php
+
+
+  include "regheader.php";
+?>
+
+
+<div class="row">
+<center>
+              <div class="col-md-8 col-xs-12 " style="float: none; margin: 0 auto;">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Send Mail </h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        
+                        
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <form class="form-horizontal form-label-left input_mask">
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control " id="inputSuccess2" placeholder="From" required="required">
+                        
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control " id="inputSuccess3" placeholder="To" required="required">
+                        
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control has-feedback-left" id="inputSuccess4" placeholder="Email">
+                        <span class="fa fa-envelope form-control-feedback left" aria-hidden="true" style="color:lightgreen"></span>
+                      </div>
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control has-feedback-left " id="inputSuccess5" placeholder="Phone">
+                        <span class="fa fa-phone form-control-feedback left" aria-hidden="true" style="color:lightgreen"></span>
+                      </div>
+
+
+
+                       
+                      <div class="form-group">
+                        
+                      </div>
+                      <div class="ln_solid"></div>
+                        <div class="x_title">
+                    <h2>Tell Us About Your Usage ! </h2>
+
+                    <div class="form-group">
+
+
+
+                    <div class="x_content">
+                    <br />
+                    <form class="form-horizontal form-label-left input_mask">
+
+
+                    <div class="col-md-6 col-sm-6 col-xs-12" style="margin-bottom: 5px;">
+                          <div id="gender" class="btn-group" data-toggle="buttons">
+                            <label class="btn btn-success" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="Domestic"> &nbsp; Domestic &nbsp;
+                            </label>
+                            <label class="btn btn-success" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="Commercial"> Commercial
+                            </label>
+                            <label class="btn btn-success" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="Industrial"> &nbsp; Industrial &nbsp;
+                            </label>
+                          </div>
+                        </div>
+                      
+                     
+                      <div class="col-md-6 col-sm-6 col-xs-12" >
+                          <div id="gender" class="btn-group" data-toggle="buttons" style="margin-bottom: 35px;">
+                            <label class="btn btn-success" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="one"> &nbsp; Single Phase &nbsp;
+                            </label>
+                            <label class="btn btn-success" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="three"> Three Phase
+                            </label>
+                          </div>
+                        </div>
+                        
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback"  >
+                        <input type="text" class="form-control has-feedback-right has-feedback-left" id="inputSuccess4" placeholder="Units">
+                        <span class="form-control-feedback right" aria-hidden="true" style="color:darkgrey">kWh</span>
+                         <span class="form-control-feedback left fa fa-lightbulb-o" aria-hidden="true" style="color:lightgreen"></span>
+                      </div>
+                      
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control has-feedback-left has-feedback-right " id="inputSuccess5" placeholder="Monthly Bill">
+                        <span class="fa fa-file-text form-control-feedback left" aria-hidden="true" style="color:lightgreen"></span>
+                         <span class=" form-control-feedback right" aria-hidden="true" style="color:darkgrey">INR</span>
+                      </div>
+                         <br>
+
+                       <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control has-feedback-left has-feedback-right" id="inputSuccess4" placeholder="Contract Load">
+                        <span class="fa fa-bolt form-control-feedback left" aria-hidden="true" style="color:lightgreen"></span>
+                        <span class="form-control-feedback right" aria-hidden="true" style="color:darkgrey">kW</span>
+                      </div>
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control has-feedback-left has-feedback-right " id="inputSuccess5" placeholder="Roof Area">
+                        <span class="fa fa-arrows form-control-feedback left" aria-hidden="true" style="color:lightgreen"></span>
+                         <span class=" form-control-feedback right" aria-hidden="true" style="color:darkgrey">sq.ft</span>
+                      </div>
+
+                      </div>
+                      
+
+
+
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Gender</label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <div id="gender" class="btn-group" data-toggle="buttons">
+                            <label class="btn btn-success" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="Domestic"> &nbsp; Domestic &nbsp;
+                            </label>
+                            <label class="btn btn-success" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="Commercial"> Commercial
+                            </label>
+                            <label class="btn btn-success" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                              <input type="radio" name="gender" value="Industrial"> &nbsp; Industrial &nbsp;
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+
+                      \
+                      <div class="form-group">
+                        
+                      </div>
+                      <div class="ln_solid"></div>
+                      <div class="form-group">
+                        <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
+                          <button type="button" class="btn btn-primary">Cancel</button>
+						   <button class="btn btn-primary" type="reset">Reset</button>
+                          <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                      </div></center>
+
+                    </form>
+                  </div>
+                </div>
+
+
+<?php
+
+
+  include "footer.php";
+?>
