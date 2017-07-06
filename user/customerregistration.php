c28d0cccdcff59f90516a41d842222b2188db1af
iterjrm <codingjrm@gmail.com>
2017-07-06 14:05:58 +0530

    fileupload

---
+++ user/customerregistration.php
@@ -0,0 +1,88 @@
+<?php
+
+
+  include "regheader.php";
+?>
+
+
+<div class="row">
+<center>
+              <div class="col-md-8 col-xs-12 " style="float: none; margin: 0 auto;">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Tell Us Something Anout Yourself ! </h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        
+                        
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <form class="form-horizontal form-label-left input_mask">
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="First Name" required="required">
+                        <span class="fa fa-user form-control-feedback left" aria-hidden="true" style="color:lightgreen"></span>
+                      </div>
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control  has-feedback-left" id="inputSuccess3" placeholder="Last Name" required="required">
+                        <span class="fa fa-user form-control-feedback left" aria-hidden="true"  style="color:lightgreen"></span>
+                      </div>
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control has-feedback-left" id="inputSuccess4" placeholder="Email">
+                        <span class="fa fa-envelope form-control-feedback left" aria-hidden="true" style="color:lightgreen"></span>
+                      </div>
+
+                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control has-feedback-left " id="inputSuccess5" placeholder="Phone">
+                        <span class="fa fa-phone form-control-feedback left" aria-hidden="true" style="color:lightgreen"></span>
+                      </div>
+
+
+
+                       
+                      <div class="form-group">
+                        
+                      </div>
+                      <div class="ln_solid"></div>
+                        <div class="x_title">
+                    <h2>Tell Us About Your Usage ! </h2>
+
+                       <div class="row">
+                      <div class="btn-group">
+                        <button class="btn btn-default" type="button">Left</button>
+                        <button class="btn btn-default" type="button">Middle</button>
+                        <button class="btn btn-default" type="button">Right</button>
+                      </div>
+
+                      <div class="form-group">
+                        
+                      </div>
+                      <div class="ln_solid"></div>
+                      <div class="form-group">
+                        <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
+                          <button type="button" class="btn btn-primary">Cancel</button>
+						   <button class="btn btn-primary" type="reset">Reset</button>
+                          <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                      </div></center>
+
+                    </form>
+                  </div>
+                </div>
+
+
+<?php
+
+
+  include "footer.php";
+?>
