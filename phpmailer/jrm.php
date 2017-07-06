aa62c6c02fefb40b940eceaa05aac78bfc679d80
iterjrm <codingjrm@gmail.com>
2017-07-06 14:57:45 +0530

    Create jrm.php

---
+++ phpmailer/jrm.php
@@ -0,0 +1,35 @@
+<?php
+require("class.PHPMailer.php");
+
+$mail = new PHPMailer();
+
+$mail->IsSMTP();                                      // set mailer to use SMTP
+$mail->Host = "mail.smartsolar.co.in";  // specify main and backup server
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->Username = "jrm@smartsolar.co.in";  // SMTP username
+$mail->Password = "sikujyoti"; // SMTP password
+
+$mail->From = "from@smartsolar.co.in";
+$mail->FromName = "Mailer";
+$mail->AddAddress("jrmkvk@gmail.com", "Josh Adams");
+$mail->AddAddress("jrmkvk@gmail.com");                  // name is optional
+$mail->AddReplyTo("info@smartsolar.co.in", "Information");
+
+$mail->WordWrap = 50;                                 // set word wrap to 50 characters
+$mail->AddAttachment("/var/tmp/file.tar.gz");         // add attachments
+$mail->AddAttachment("/tmp/image.jpg", "new.jpg");    // optional name
+$mail->IsHTML(true);                                  // set email format to HTML
+
+$mail->Subject = "Here is the subject";
+$mail->Body    = "This is the HTML message body <b>in bold!</b>";
+$mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+
+if(!$mail->Send())
+{
+   echo "Message could not be sent. <p>";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+
+echo "Message has been sent";
+?>
