51840b5bd7c37a6bd71388b086fe99eb902a0f4a
subhralulu <rahullulu1998@gmail.com>
2017-07-08 01:44:44 +0530

    connect

--- php/connect.php
+++ php/connect.php
@@ -1,13 +1,10 @@
 <?php
 
-
-
-
 	$conn=new mysqli("localhost","smartzts_jrm","sikujyoti753","smartzts_ssjrm");
 
-	if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
+	if ($conn->connect_error) 
+	{
+    	die("Connection failed: " . $conn->connect_error);
+	} 
 
 ?>
