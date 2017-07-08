b1ac69476edd72df80e3c21e571d071c6825f38e
iterjrm <codingjrm@gmail.com>
2017-07-08 17:40:20 +0530

    a

--- user/customerregistration.php
+++ user/customerregistration.php
@@ -25,7 +25,7 @@
                   </div>
                   <div class="x_content">
                     <br />
-                    <form class="form-horizontal form-label-left input_mask" action="custregcontroller.php" method="post">
+                    <form class="form-horizontal form-label-left input_mask" action="../php/custregcontroller.php" method="post">
 
                       <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
                         <input type="text" name="firstname" class="form-control has-feedback-left" id="firstname" placeholder="First Name" required="required">
