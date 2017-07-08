1b92f61f6389b676142f828cc131635b0f18c04e
sidharthsray <sidharthsray@gmail.com>
2017-07-08 18:46:39 +0530

    ss

--- user/addemployee.php
+++ user/addemployee.php
@@ -107,11 +107,12 @@
 
                      </div>
                      <br>
-                     <div class="form-group">
-                    <div class="col-md-9 col-sm-9 col-xs-12">
-                    	 <label for="pa" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Permanent Address</span></label>
-                      <textarea class="pa form-control" placeholder="Permanent Address">
+                     <div class="col-md-9 col-sm-9 col-xs-12">
+      		              	 <label for="pa" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Permanent Address</span></label>
+            		          <textarea class="pa form-control" placeholder="Permanent Address">
                     </div>
+
+
                   </div>
 
 
