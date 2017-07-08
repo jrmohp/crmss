36dfb488aa1e0b02d5582608d5981dc28b583901
iterjrm <codingjrm@gmail.com>
2017-07-08 11:08:08 +0530

    a

--- user/checkdesign.php
+++ user/checkdesign.php
@@ -4,7 +4,8 @@
   include "header.php";
 ?>
 
-
+<div class="x_title">
+                    <h2>Send Email  <small>specified customer</small></h2>
 
 <?php
 
--- user/header.php
+++ user/header.php
@@ -280,7 +280,23 @@
          
         
         <!-- page content -->
- 
+        
+<center>
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
 
 
 
