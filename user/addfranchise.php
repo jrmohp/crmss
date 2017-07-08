f3c983bd68ab82b04b9ca4c47aa0b1bab9ac56d3
sidharthsray <sidharthsray@gmail.com>
2017-07-08 23:40:45 +0530

    ss

--- user/addfranchise.php
+++ user/addfranchise.php
@@ -25,7 +25,29 @@
                 	<h2>Franchise Registration</h2>
                     <div class="row" style="margin: 0.5%">
                     	 <div class="ln_solid"></div>
-                   			 <h2>Owner Details</h2 >   
+                   			 <h2>Owner Details</h2 >
+
+
+   			  <div class="col-md-4 col-sm-4 col-xs-12"  >
+                  		   <label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">First Name</span></label>
+
+                    			<input type="text" name="fname" id="fname" class="form-control" placeholder="First Name">			
+                   			 </div>
+
+
+
+                  		  <div class="col-md-4 col-sm-4 col-xs-12" >
+                         <label for="mname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Middle Name</span></label>
+
+                   		  	<input type="text" name="mname" id="mname" class="form-control" placeholder="Middle Name">
+                  		  </div>
+
+
+                      
+                   		 <div class="col-md-4 col-sm-4 col-xs-12"  >
+                    	   <label for="lname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Last Name</span></label>
+							<input type="text" name="lname" id="lname" class="form-control"  placeholder="Last Name">	
+                    	</div>   
 
                
 
