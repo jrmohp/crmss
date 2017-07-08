71fd667c127379767897e10ff85801338379fcd2
iterjrm <codingjrm@gmail.com>
2017-07-08 17:06:08 +0530

    Merge branch 'test' of https://github.com/iterjrm/crmss into test

--- user/addemployee.php
+++ user/addemployee.php
@@ -29,13 +29,17 @@
 
                     	<input type="text" name="fname" id="fname" class="form-control" placeholder="First Name">			
                     </div>
-                     <div class="col-md-4 col-sm-4 col-xs-12">
+
+                         <label for="mname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Middle Name</span></label>
+                    <div class="col-md-3 col-sm-3 col-xs-11" style="margin-left:-2.5%">
                     
-                     	<input type="text" name="mname" id="mname" class="form-control" placeholder="Mi">
+                     	<input type="text" name="mname" id="mname" class="form-control" placeholder="Middle Name">
                     </div>
-                    <div class="col-md-4 col-sm-4 col-xs-12">
+
+                         <label for="lname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Last Name</span></label>
+                    <div class="col-md-3 col-sm-3 col-xs-11"  style="margin-left:-2.5%">
                     
-						<input type="text" name="lname" id="lname" class="form-control" >	
+						<input type="text" name="lname" id="lname" class="form-control"  placeholder="Last Name">	
                     </div>
                     	
                     	
