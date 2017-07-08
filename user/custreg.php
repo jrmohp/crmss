021d5bf86ca88c482a974b004b27d967325e6e27
iterjrm <codingjrm@gmail.com>
2017-07-08 17:31:11 +0530

    k
    sd

---
+++ user/custreg.php
@@ -0,0 +1,27 @@
+<?php
+
+
+  include "regheader.php";
+?>
+
+
+<div class="row">
+<center>
+              <div class="col-md-8 col-xs-12 " style="float: none; margin: 0 auto;">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Tell Us Something About Yourself ! </h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        
+                        
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
