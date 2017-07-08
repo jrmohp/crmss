84d431240c5c595e90a80a68b2f5ab86d342c110
sidharthsray <sidharthsray@gmail.com>
2017-07-08 17:04:12 +0530

    as

--- user/addemployee.php
+++ user/addemployee.php
@@ -25,13 +25,13 @@
                     <div class="row">
 
                     	 <label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">First Name</span></label>
-                    <div class="col-md-3 col-sm-3 col-xs-11" style="margin-left:-2.5%">
+                    <div class="col-md-3 col-sm-3 col-xs-11"  style="margin-left:-2.5%">
 
                     	<input type="text" name="fname" id="fname" class="form-control" placeholder="First Name">			
                     </div>
 
                          <label for="mname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Middle Name</span></label>
-                    <div class="col-md-3 col-sm-3 col-xs-11" style="margin-left:-2.5%">
+                    <div class="col-md-3 col-sm-3 col-xs-11"  style="margin-left:-2.5%">
                     
                      	<input type="text" name="mname" id="mname" class="form-control" placeholder="Middle Name">
                     </div>
