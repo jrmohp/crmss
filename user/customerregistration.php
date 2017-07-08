a58c6e6f2e2568823ddd1f6ab299ec7d8e147899
iterjrm <codingjrm@gmail.com>
2017-07-08 18:54:21 +0530

    a

--- user/customerregistration.php
+++ user/customerregistration.php
@@ -38,7 +38,7 @@
                       </div>
 
                       <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                        <input type="text" name="email" class="form-control has-feedback-left" id="email" placeholder="Email" onchange="checkemail()">
+                        <input type="text" name="email" class="form-control has-feedback-left" id="email" placeholder="Email" onkeyup="checkemail()">
                         <span class="fa fa-envelope form-control-feedback left" aria-hidden="true" style="color:lightgreen"></span>
                         <span id="email_status" style="color: red"> </span> 
                       </div>
