9f4d0b14fa88be4fb378f4f3a9c456bd075a5a99
iterjrm <codingjrm@gmail.com>
2017-07-08 10:26:51 +0530

    s

--- user/singlemailnew2.php
+++ user/singlemailnew2.php
@@ -10,7 +10,6 @@
           
 
               
-            </div>
             <div class="clearfix"></div>
             <div class="row">
               <div class="col-md-12 col-sm-12 col-xs-12">
