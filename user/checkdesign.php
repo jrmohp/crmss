1029cb1b24da72765a8329d5393323338e86ab85
iterjrm <codingjrm@gmail.com>
2017-07-08 11:11:33 +0530

    a

--- user/checkdesign.php
+++ user/checkdesign.php
@@ -4,7 +4,16 @@
   include "header.php";
 ?>
 
-	<div class="col-md-12 col-sm-12 col-xs-12">
+
+
+
+<div class="right_col" role="main" style="min-height: 200px">
+          
+
+              
+            <div class="clearfix"></div>
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
                     <h2>Send Email  <small>specified customer</small></h2>
@@ -64,7 +73,14 @@
                     </div>
                     </form>
                     </div>
-           
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    
+                    
+
+	
 <?php
 
 
