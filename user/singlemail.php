4f4ab70da53a10ccaa27d03ae6312e865ca74a9e
Crypto <kamakhya166@gmail.com>
2017-07-08 15:35:06 +0530

    copied from master

--- user/singlemail.php
+++ user/singlemail.php
@@ -15,7 +15,7 @@
 </head>
 <body>
 
- 	<div class="row" id="body2">
+    <div class="row" id="body2">
             <div class="col-md-12 col-sm-12 col-xs-12">
             
             
@@ -27,21 +27,21 @@
                         <div id="querymsg"></div>
 
                     <div class="row">
-                    	
-                    	<label class="control-label col-sm-1 align-middle" for="mfrom" style="margin:-5px"><h2><span class="label label-info">From</span></h2></label>
-                    	<div class="col-md-5 col-sm-6 col-xs-10">
+                        
+                        <label class="control-label col-sm-1 align-middle" for="mfrom" style="margin:-5px"><h2><span class="label label-info">From</span></h2></label>
+                        <div class="col-md-5 col-sm-6 col-xs-10">
 
-                    	<input type="text"  class="form-control"  name="mfrom" placeholder="From" id="mfrom" readonly="true" value="jrm@smartsolar.co.in">
-                    		
-                    	</div>
+                        <input type="text"  class="form-control"  name="mfrom" placeholder="From" id="mfrom" readonly="true" value="jrm@smartsolar.co.in">
+                            
+                        </div>
 
-                    	<label class="control-label col-sm-1 align-middle" for="mto" style="margin:-5px"><h2><span class="label label-info">&nbspTo&nbsp</span></h2></label>
-                    	<div class="col-md-5 col-sm-5 col-xs-10">
-                    		
-                    	<input type="text"  class="form-control"  name="mto" id="mto" placeholder="To" readonly="true" value="iterjrm@gmail.com">
-                    		
-                    	</div>
-                    	
+                        <label class="control-label col-sm-1 align-middle" for="mto" style="margin:-5px"><h2><span class="label label-info">&nbspTo&nbsp</span></h2></label>
+                        <div class="col-md-5 col-sm-5 col-xs-10">
+                            
+                        <input type="text"  class="form-control"  name="mto" id="mto" placeholder="To" readonly="true" value="iterjrm@gmail.com">
+                            
+                        </div>
+                        
 
                     </div>
                     <br>
@@ -50,13 +50,13 @@
                     <div class="row">
 
 
-                    	<label class="control-label col-sm-1 align-middle" for="subject" style="margin:-5px"><h2><span class="label label-info">Subject</span></h2></label>
+                        <label class="control-label col-sm-1 align-middle" for="subject" style="margin:-5px"><h2><span class="label label-info">Subject</span></h2></label>
 
-                    	<div class="col-md-11 col-sm-11 col-xs-11">
-                    		
-                    	<input type="text"  class="form-control"  name="subject" id="subject" placeholder="Subject" data-parsley-required>
-                    		
-                    	</div>
+                        <div class="col-md-11 col-sm-11 col-xs-11">
+                            
+                        <input type="text"  class="form-control"  name="subject" id="subject" placeholder="Subject" data-parsley-required>
+                            
+                        </div>
                     </div>
                     <br>
                     <br>
@@ -154,7 +154,7 @@
 
 
 </script>
-	
+    
 <?php
 
 
