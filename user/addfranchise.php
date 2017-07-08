0fa8b1ac5093290c10ea93f75ef0f27a87f697c3
sidharthsray <sidharthsray@gmail.com>
2017-07-09 00:20:43 +0530

    ss

--- user/addfranchise.php
+++ user/addfranchise.php
@@ -55,33 +55,101 @@
 
              <div class="row">
 
-              <div class="col-md-4 col-sm-4 col-xs-12"  >
-                  		   <label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Owner First Name</span></label>
+ 				 <div class="col-md-4 col-sm-4 col-xs-12"  >
+              		   		<label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Owner First Name</span></label>
 
-                    			<input type="text" name="fname" id="fname" class="form-control" placeholder="Owner First Name">			
-                   			 </div>
+                			<input type="text" name="fname" id="fname" class="form-control" placeholder="Owner First Name">			
+       			 </div>
 
 
 
-                  		  <div class="col-md-4 col-sm-4 col-xs-12" >
-                         <label for="mname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Owner Middle Name</span></label>
+          		  	<div class="col-md-4 col-sm-4 col-xs-12" >
+                    			 <label for="mname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Owner Middle Name</span></label>
 
-                   		  	<input type="text" name="mname" id="mname" class="form-control" placeholder="Owner Middle Name">
-                  		  </div>
+               		  	 		 <input type="text" name="mname" id="mname" class="form-control" placeholder="Owner Middle Name">
+  		 		 	</div>
 
 
                       
-                   		 <div class="col-md-4 col-sm-4 col-xs-12"  >
-                    	   <label for="lname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Owner Last Name</span></label>
+               		 	<div class="col-md-4 col-sm-4 col-xs-12"  >
+                    	   			<label for="lname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Owner Last Name</span></label>
 							<input type="text" name="lname" id="lname" class="form-control"  placeholder="Owner Last Name">	
                     	</div>   
 
-             </div>
              </div>
 
              <br>
 
              <div class="row">
+             		<div class="col-md-6 col-sm-6 col-xs-12" >
+                  					   <label for="fname" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Owner Id</span></label>
+
+                    						<input type="text" name="fname" id="fname" class="form-control" placeholder="Owner Id">			
+                   			
+					</div>			
+									<div class="col-md-6 col-sm-6 col-xs-12" >
+                  					   <label for="mname" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Pan No.</span></label>
+
+                    						<input type="text" name="mname" id="mname" class="form-control" placeholder="Pan No.">			
+                   			
+									</div>
+
+
+
+             </div>
+
+			<br>
+
+         	<div class="row">
+             		<div class="col-md-6 col-sm-6 col-xs-12" >
+      					   	<label for="fname" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Email Id</span></label>
+
+    						<input type="text" name="fname" id="fname" class="form-control" placeholder="Email Id">			
+                   			
+					</div>			
+							<div class="col-md-6 col-sm-6 col-xs-12" >
+          					   		<label for="mname" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="<br>
+
+         	<div class="row">
+             		<div class="col-md-6 col-sm-6 col-xs-12" >
+      					   	<label for="fname" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Email Id</span></label>
+
+    						<input type="text" name="fname" id="fname" class="form-control" placeholder="Email Id">			
+                   			
+					</div>			label label-info">Contact No.</span></label>
+
+            						<input type="text" name="mname" id="mname" class="form-control" placeholder="Contact No.">			
+                   			
+							</div>
+
+
+
+         	</div>
+
+         	<br>
+
+         	<div class="row">
+             		<div class="col-md-12 col-sm-12 col-xs-12" >
+      					   	<label for="fname" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">GST</span></label>
+
+    						<input type="text" name="fname" id="fname" class="form-control" placeholder="GST">			
+                   			
+					</div>
+
+			<br>
+
+
+
+                     <div class="row">
+                     		</div class="col-md-9 col-sm-9 col-xs-12">
+      		              	 <label for="pa" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Address</span></label>
+            		          <textarea class="pa form-control" placeholder="Address" rows="5"></textarea>
+                     			</div>
+                     </div>
+                    			
+
+
+
 
 
 
