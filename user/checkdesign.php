dfea78ebfdc41a4a13497019db50325ede63947e
iterjrm <codingjrm@gmail.com>
2017-07-08 11:28:03 +0530

    a

--- user/checkdesign.php
+++ user/checkdesign.php
@@ -7,9 +7,7 @@
 
 
 
- <div class="row">
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
+ 
                   <div class="x_title">
                     <h2>Send Email  <small>specified customer</small></h2>
                     <ul class="nav navbar-right panel_toolbox">
@@ -70,7 +68,7 @@
                     </div>
                     </div>
                     </div>
-
+ 
 
 	
 <?php
