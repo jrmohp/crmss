cf30ff82ca2fd15abf2c883b41b6c2264d627902
sidharthsray <sidharthsray@gmail.com>
2017-07-08 20:09:44 +0530

    ss

--- user/addemployee.php
+++ user/addemployee.php
@@ -68,7 +68,11 @@
 
 
                      </div>
+
+
                      <br>
+
+
                      <div class="row">
                     				
                   					  <div class="col-md-6 col-sm-6 col-xs-12" >
@@ -87,7 +91,11 @@
 
 
                      </div>
+
+
                      <br>
+
+
                      <div class="row">
                     				
                   					  <div class="col-md-6 col-sm-6 col-xs-12" >
@@ -106,7 +114,11 @@
 
 
                      </div>
+
+
                      <br>
+
+
 
                      <div class="row">
                      		</div class="col-md-9 col-sm-9 col-xs-12">
@@ -117,10 +129,13 @@
 
 	                
 	                 <br>
+
+
 	                 <div class="row">
-	                 </div class="col-md-9 col-sm-9 col-xs-12">
+	                 		</div class="col-md-9 col-sm-9 col-xs-12">
 	      	              	 <label for="ta" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Temporary Address</span></label>
 	           		          <textarea class="ta form-control" placeholder="Temporary Address" rows="5"></textarea>
+	           		          </div>
 	                 </div>
 
 
