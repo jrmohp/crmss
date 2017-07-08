7be57186bbe7c2786c2d3e1b6806c3f13acf3383
iterjrm <codingjrm@gmail.com>
2017-07-08 17:36:59 +0530

    m

--- user/custreg.php
+++ user/custreg.php
@@ -32,7 +32,7 @@
                       
                     <div class="col-xs-12 col-md-5 col-sm-5">
 
-                    <input type="text" name="firstname" class="form-control has-feedback-left" id="firstname" placeholder="First Name" data-parsley-required>
+                    <input type="text" name="firstname" class="form-control " id="firstname" placeholder="First Name" data-parsley-required>
                       
                     </div>
 
