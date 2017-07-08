5b2682175a756d6599009f8a882678577ea059a4
sidharthsray <sidharthsray@gmail.com>
2017-07-08 18:53:34 +0530

    ss

--- user/addemployee.php
+++ user/addemployee.php
@@ -110,11 +110,15 @@
                      </div class="col-md-9 col-sm-9 col-xs-12">
       		              	 <label for="pa" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Permanent Address</span></label>
             		          <textarea class="pa form-control" placeholder="Permanent Address" rows="5"></textarea>
-                    </div>
+                     </div>
 
 
                   </div>
-
+                   <br>
+                    </div class="col-md-9 col-sm-9 col-xs-12">
+      		              	 <label for="ta" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Temporary Address</span></label>
+            		          <textarea class="ta form-control" placeholder="Temporary Address" rows="5"></textarea>
+                     </div>
 
 
 
