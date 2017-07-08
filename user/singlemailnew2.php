b8ed2913ebb5610d5ec97e02ae2bcfdde2c7b095
iterjrm <codingjrm@gmail.com>
2017-07-08 09:44:08 +0530

    a

--- user/singlemailnew2.php
+++ user/singlemailnew2.php
@@ -4,7 +4,31 @@
   include "header.php";
 ?>
 
-	 <form class="form-horizontal form-label-left input_mask">
+	<div class="row">
+              <div class="col-md-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Form Design <small>different form elements</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <form class="form-horizontal form-label-left input_mask">
 
                       <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
                         <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="First Name">
@@ -61,9 +85,8 @@
                       </div>
 
                     </form>
-
-
-
+                  </div>
+                </div>
 	<?php
 
 
