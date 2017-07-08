b88c78b79dc5be402ba617fecfbdd44ecf7d4c14
sidharthsray <sidharthsray@gmail.com>
2017-07-08 16:43:17 +0530

    s

--- user/addemployee.php
+++ user/addemployee.php
@@ -24,7 +24,7 @@
 
                     <div class="row">
 
-                    	 <label for="fname" class="col-md-1 col-sm-1 col-xs-1">kjdsbfkj</label>
+                    	 <label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">First Name</span></label>
                     <div class="col-md-3 col-sm-3 col-xs-11">
 
                     	<input type="text" name="fname" id="fname" class="form-control" placeholder="First Name">			
