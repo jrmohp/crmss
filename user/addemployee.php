1a00060a69e1d4811aabe4b4d663d08681369cd6
sidharthsray <sidharthsray@gmail.com>
2017-07-08 19:20:01 +0530

    s

--- user/addemployee.php
+++ user/addemployee.php
@@ -126,7 +126,22 @@
 
 
                    
-	                <div class="row">sdifujb</div>
+
+                    <div class="row" style="margin:1%">
+                    	 <div class="ln_solid"></div>
+                   			 <h2>Basic Information</h2>
+
+                    	
+                  		  <div class="col-md-12 col-sm-12 col-xs-12"  >
+                  		   <label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">First Name</span></label>
+
+                    			<input type="text" name="fname" id="fname" class="form-control" placeholder="First Name">			
+                   			 </div>
+                   			 </div>
+
+
+
+
 
 
                     </form>
