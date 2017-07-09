3f4b24fc7c8978d95e6e929e75f9a93aa675d0a6
Crypto <kamakhya166@gmail.com>
2017-07-10 01:53:39 +0530

    sadasd

--- user/addemployee.php
+++ user/addemployee.php
@@ -259,7 +259,7 @@
                    			 </div>
 
 	                 </div>
-
+                  <br>
                     <div class="row">
                         
                         <div class="col-md-4 col-sm-4 col-xs-12"  >
@@ -281,9 +281,10 @@
                          </div>
 
                    </div>
+                  <br>
                   <div class="row">
                     <div class="col-md-12 col-sm-12 col-xs-12">
-                    <button  class="btn btn-success col-md-4 " type="submit"  id="sendmail">Send</button>
+                    <button  class="btn btn-success col-md-12 " type="submit"  id="sendmail">Send</button>
                     </div>
                   </div>
 
