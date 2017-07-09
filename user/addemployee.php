c06569e883cb212d128ea09387664dd887df4330
Crypto <kamakhya166@gmail.com>
2017-07-09 22:57:22 +0530

    lol

--- user/addemployee.php
+++ user/addemployee.php
@@ -19,7 +19,7 @@
             <div class="col-md-12 col-sm-12 col-xs-12">
 
             
-                    <form  method="post" data-parsley-validate action="addemployeecontroller.php">
+                    <form  method="post" data-parsley-validate action="../php/addemployeecontroller.php">
                     	
 
                 	<h2>Employee Registration</h2>
