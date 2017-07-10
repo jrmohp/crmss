82f83bff619d4a25b0c5304469bbd8745cdce25b
iterjrm <codingjrm@gmail.com>
2017-07-11 00:21:06 +0530

    x

--- user/viewsite.php
+++ user/viewsite.php
@@ -3,22 +3,11 @@
 require("../php/connect.php");
 
 
-$query = mysql_query("SELECT property FROM siteid WHERE id='SITE0001'"); 
+$query ="SELECT property FROM siteid WHERE id='SITE0001'"; 
 
-while($row=mysql_fetch_array($query))
-{
-echo sizeof($row);
-$length= sizeof($row);
-echo "hela";
+$result=mysqli_query($con,$query);
+$row=mysqli_fetch_array($result,MYSQLI_NUM);
 
-/*foreach($row as $value)
-{
-	echo "<label for="$value">".$value."</label>";
-    echo "<input type="text" name="$value" id="$value">";
-    echo "<br>;
-}*/
-}
-
-
+echo $row[0];
 
 ?>
