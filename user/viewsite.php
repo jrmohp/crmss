7df3ee489f90d41ef814799545040514b8a59935
iterjrm <codingjrm@gmail.com>
2017-07-11 00:15:08 +0530

    a

--- user/viewsite.php
+++ user/viewsite.php
@@ -10,12 +10,12 @@ while($row=mysql_fetch_array($query))
 echo sizeof($row);
 $length= sizeof($row);
 
-foreach($row as $value)
+/*foreach($row as $value)
 {
 	echo "<label for="$value">".$value."</label>";
     echo "<input type="text" name="$value" id="$value">";
     echo "<br>;
-}
+}*/
 }
 
 
