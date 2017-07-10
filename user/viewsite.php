dd5f99f6b1aa31b2818043451ad99feee1ad2e26
iterjrm <codingjrm@gmail.com>
2017-07-11 00:22:32 +0530

    a

--- user/viewsite.php
+++ user/viewsite.php
@@ -5,7 +5,7 @@ require("../php/connect.php");
 
 $query ="SELECT property FROM siteid WHERE id='SITE0001'"; 
 
-$result=mysqli_query($con,$query);
+$result=mysqli_query($conn,$query);
 $row=mysqli_fetch_array($result,MYSQLI_NUM);
 echo "hauchi";
 echo $row[0];
