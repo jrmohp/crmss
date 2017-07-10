49b25a8dd6d472280a650c4c0e69a0a1c567689d
iterjrm <codingjrm@gmail.com>
2017-07-11 00:34:36 +0530

    h

--- user/viewsite.php
+++ user/viewsite.php
@@ -3,7 +3,7 @@
 require("../php/connect.php");
 
 
-$sql ="SELECT property FROM siteid WHERE siteid='SITE0001'"; 
+$query ="SELECT property FROM siteid WHERE siteid='SITE0001'"; 
 
 /*
 if($result=mysqli_query($conn,$sql))
