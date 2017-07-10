9570c2a763f34b664fcb2543ffc7957f369b8b26
Crypto <kamakhya166@gmail.com>
2017-07-11 01:30:22 +0530

    sda

--- user/viewsite.php
+++ user/viewsite.php
@@ -34,7 +34,7 @@ if($result=mysqli_query($conn,$query))
     				echo'<div class="col-md-12 col-sm-12 col-xs-12"  >
               		  	<label for="$val" class="col-md-4 col-sm-4 col-xs-4"><span class="label label-info">'.$val.'</span></label>
 
-            			<input type="text" name="$val" id="$val" class="form-control" placeholder="$val">			
+            			<input type="text" name="$val" id="$val" class="form-control" placeholder='''$val'''>			
     				</div><br>';
 
     	}
