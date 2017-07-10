2db6f46b5190257a7bd3a890ca5c6af207893586
iterjrm <codingjrm@gmail.com>
2017-07-11 00:31:21 +0530

    d

--- user/viewsite.php
+++ user/viewsite.php
@@ -7,7 +7,10 @@ $sql ="SELECT property FROM siteid WHERE siteid='SITE0001'";
 
 if($result=mysqli_query($conn,$sql))
 {
-	printf ("%s (%s)\n",$row[0],$row[1]);
+	 while ($row=mysqli_fetch_row($result))
+    {
+    printf ("%s (%s)\n",$row[0],$row[1]);
+    }
 
 }
 ?>
