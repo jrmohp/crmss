4ce77a0fa4896effb4fc8b69b8eccad508557758
iterjrm <codingjrm@gmail.com>
2017-07-11 00:39:34 +0530

    hh

--- user/viewsite.php
+++ user/viewsite.php
@@ -6,11 +6,12 @@ require("../php/connect.php");
 $query ="SELECT property FROM siteid WHERE siteid='SITE0001'"; 
 
 
-if($result=mysqli_query($conn,$sql))
+if($result=mysqli_query($conn,$query))
 {
 	 while ($row=mysqli_fetch_row($result))
     {
-   			
+   		echo $row[0];
+   		echo $row[1];	
    		printf ("%s (%s)\n",$row[0],$row[1]);
     }
 
