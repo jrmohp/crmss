1134e977774b2b1182eef2950b857362ac081723
Crypto <kamakhya166@gmail.com>
2017-07-11 00:54:34 +0530

    asd

--- user/viewsite.php
+++ user/viewsite.php
@@ -1,3 +1,22 @@
+<?php
+
+
+  include "header.php";
+?>
+
+
+
+
+ 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Add Faculty | Mega ATV Championship  </title>
+</head>
+<body>
+
+ 	<div class="row" id="body2">
+            <div class="col-md-12 col-sm-12 col-xs-12">
 
 
 <table>
@@ -26,3 +45,13 @@ if($result=mysqli_query($conn,$query))
 
 ?>
 </table>
+</div>
+                    </div>
+                    </body>
+
+	
+<?php
+
+
+  include "footer.php";
+?>
