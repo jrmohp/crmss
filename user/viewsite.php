438fc588a9eab9350a871a4666126f0aa765722e
iterjrm <codingjrm@gmail.com>
2017-07-11 00:33:06 +0530

    a

--- user/viewsite.php
+++ user/viewsite.php
@@ -3,14 +3,27 @@
 require("../php/connect.php");
 
 
-$sql ="SELECT property FROM siteid WHERE siteid='SITE0001'"; 
+$query ="SELECT property FROM siteid WHERE siteid='SITE0001'"; 
 
+/*
 if($result=mysqli_query($conn,$sql))
 {
 	 while ($row=mysqli_fetch_row($result))
     {
-    printf ("%s (%s)\n",$row[0],$row[1]);
+   		
     }
 
-}
+}*/
++while($row=mysql_fetch_array($query))
++{
++echo sizeof($row);
++$length= sizeof($row);
++
++foreach($row as $value)
++{
++	echo "<label for="$value">".$value."</label>";
++    echo "<input type="text" name="$value" id="$value">";
++    echo "<br>;
++}
++}
 ?>
