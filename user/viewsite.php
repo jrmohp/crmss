ce4242fc59a73e23cfba20d899053d2b0a6f8e74
Crypto <kamakhya166@gmail.com>
2017-07-11 01:14:56 +0530

    asd

--- user/viewsite.php
+++ user/viewsite.php
@@ -31,8 +31,8 @@ if($result=mysqli_query($conn,$query))
    		echo"<div class="row"> <div class="ln_solid"></div>";
     	foreach ($row as $val) 
     	{	
-    				echo"<div class="col-md-6 col-sm-6 col-xs-12"  >
-              		  	<label for="$val" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">".$val."</span></label>";
+    				echo"<div class="col-md-12 col-sm-12 col-xs-12"  >
+              		  	<label for="$val" class="col-md-4 col-sm-4 col-xs-4"><span class="label label-info">".$val."</span></label>";
 
             			echo"<input type="text" name="$val" id="$val" class="form-control" placeholder="$val">			
     				</div><br>";
