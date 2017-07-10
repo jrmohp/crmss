6ee2ec4839b3e614fb5012025c8c82a352d8c5ac
iterjrm <codingjrm@gmail.com>
2017-07-11 00:02:28 +0530

    a

--- user/addsite.php
+++ user/addsite.php
@@ -90,8 +90,8 @@ function regc()
 {
   alert("hauchi");
   var ec=$('#ecount').val();
-  var divi=document.getElementById("ddata");
-var c=0;
+  
+var c=1;
 var s1="<div class='row'><div class='col-md-6 col-sm-6 col-xs-12' ><label for='mname' class='col-md-1 col-sm-1 col-xs-1'><span class='label label-info'>Expense";
 var s2="</span></label><input type='text' name='prop";
 var s3="id='prop";
@@ -103,7 +103,7 @@ while(c<ec)
 {
 
 
-  divi.innerHTML=totals;
+  $('ddata').append(totals);
   c++;
 
 }
