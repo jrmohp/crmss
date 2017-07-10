d427a197fb6accec89ec95649ff01e4514ff0fa1
iterjrm <codingjrm@gmail.com>
2017-07-11 00:29:46 +0530

    J

--- user/viewsite.php
+++ user/viewsite.php
@@ -5,5 +5,9 @@ require("../php/connect.php");
 
 $sql ="SELECT property FROM siteid WHERE id='SITE0001'"; 
 
+if($result=mysqli_query($conn,$sql))
+{
+	printf ("%s (%s)\n",$row[0],$row[1]);
 
+}
 ?>
