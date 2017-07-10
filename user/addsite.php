dadf38d3f3cee0526a9d9ec6467e74dc2619e2a4
iterjrm <codingjrm@gmail.com>
2017-07-11 00:04:16 +0530

    a

--- user/addsite.php
+++ user/addsite.php
@@ -94,7 +94,7 @@ function regc()
 var c=1;
 var s1="<div class='row'><div class='col-md-6 col-sm-6 col-xs-12' ><label for='mname' class='col-md-1 col-sm-1 col-xs-1'><span class='label label-info'>Expense";
 var s2="</span></label><input type='text' name='prop";
-var s3="id='prop";
+var s3="'id='prop";
 var s4="'class='form-control' placeholder='Expense Name'></div></div>";
 
 var totals=s1+c+s2+c+s3+c+s4;
