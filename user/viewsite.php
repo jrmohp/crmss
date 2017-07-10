86cc11d7510e5bbdb7ffd535c67e1a5f6895b022
iterjrm <codingjrm@gmail.com>
2017-07-11 00:27:22 +0530

    a

--- user/viewsite.php
+++ user/viewsite.php
@@ -3,18 +3,17 @@
 require("../php/connect.php");
 
 
-$query ="SELECT property FROM siteid WHERE id='SITE0001'"; 
+$sql ="SELECT property FROM siteid WHERE id='SITE0001'"; 
 
 /*$result=mysqli_query($conn,$query);
 $row=mysqli_fetch_array($result,MYSQLI_NUM);
 echo "hauchire";
 echo $row[0];*/
 
-
-if($conn->mysqli_query($query))
+if($conn->mysqli_query($sql))
 
 {
-	echo $conn->mysqli_query($query);
+	echo "hela";
 }
 else
 {
