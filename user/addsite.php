43a0f2a874d4daafcf4d2107a70778cee0965868
sidharthsray <sidharthsray@gmail.com>
2017-07-10 16:52:57 +0530

    ss

--- user/addsite.php
+++ user/addsite.php
@@ -16,44 +16,53 @@
 <body>
 
  	<div class="row" id="body2">
-            <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="col-md-12 col-sm-12 col-xs-12">
 
             
-               	<form  method="post" data-parsley-validate>
+       		<form  method="post" data-parsley-validate>
                     	
 
-				<h2>Add Site</h2>
-	    <div class="row">
-	 		<div class="ln_solid"></div>             
+			<h2>Add Site</h2>
 
 
-		  	<div class="col-md-6 col-sm-6 col-xs-12"  >
-                  		   <label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Site Id</span></label>
-
-                    			<input type="text" name="fname" id="fname" class="form-control" placeholder="Site Id">			
-	    	</div>
+    		<div class="row">
 
 
+				<div class="ln_solid"></div>             
 
-                  		  <div class="col-md-6 col-sm-6 col-xs-12" >
-                         <label for="mname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Site Name</span></label>
+
+	  				<div class="col-md-6 col-sm-6 col-xs-12"  >
+              		  	<label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Site Id</span></label>
+
+            			<input type="text" name="fname" id="fname" class="form-control" placeholder="Site Id">			
+    				</div>
+
+
+
+	  				<div class="col-md-6 col-sm-6 col-xs-12" >
+                     	<label for="mname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Site Name</span></label>
 
                    		  	<input type="text" name="mname" id="mname" class="form-control" placeholder="Site Name">
-  		  	</div>
+       		  		</div>
+
+  			</div>
 
   		  	<br>
 
 			<div class="row">
 
-			<div class="col-md-6 col-sm-6 col-xs-12"  >
-                  		   <label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Supervisor</span></label>
+					<div class="col-md-12 col-sm-12 col-xs-12"  >
+          		   		<label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Supervisor</span></label>
 
-                    			<input type="text" name="fname" id="fname" class="form-control" placeholder="Supervisor">			
-	    	</div>
+            			<input type="text" name="fname" id="fname" class="form-control" placeholder="Supervisor">			
+	    			</div>
 
 			</div>
-			</div>
+
+			<br>
+
 			
+
 
 
 
