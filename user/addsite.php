a10fbb4d54678a9cf4fd2d77f829c277124bd194
sidharthsray <sidharthsray@gmail.com>
2017-07-10 15:29:33 +0530

    ss

--- user/addsite.php
+++ user/addsite.php
@@ -40,7 +40,19 @@
 
                    		  	<input type="text" name="mname" id="mname" class="form-control" placeholder="Site Name">
                   		  </div>
- 	</div>		
+ 	</div>	
+
+
+ 		<div class="row">
+
+			<div class="col-md-6 col-sm-6 col-xs-12"  >
+                  		   <label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Supervisor</span></label>
+
+                    			<input type="text" name="fname" id="fname" class="form-control" placeholder="Supervisor">			
+	    	</div>
+
+	    </div>
+
 
 
 
