5412300ed1bbfc591262f96e6db1c5f99303cd6b
Crypto <kamakhya166@gmail.com>
2017-07-11 23:43:12 +0530

    asdd

--- php/custregcontroller.php
+++ php/custregcontroller.php
@@ -1,5 +1,9 @@
 <?php
+
 	require("connect.php");
+	
+	if(!empty($_POST['firstname']) && !empty($_POST['lastname'])&& !empty($_POST['email']) && !empty($_POST['phone'])&& !empty($_POST['city'])&& !empty($_POST['type']))	
+	{
 	$firstname=$_POST['firstname'];
 	$lastname=$_POST['lastname'];
 	$email=$_POST['email'];
@@ -13,20 +17,45 @@
 	$roofarea=$_POST['roofarea'];
 	$phase=$_POST['phase'];
 
-
-
-
 	$query = $conn->query("SELECT MAX(id) FROM user"); // execute
 	$max_id = $query->fetch_array(); // fetch
 		$tid=$max_id[0];
 		$new_id=$tid+1;
 		$ssid = 'SS' . sprintf ( "%04d" , $new_id ) ;
 
-
 	$sql= "INSERT INTO user(username,type,firstname,lastname,email,mobile,peraddress,city,mbill,cload,rarea,phase) VALUES ('$ssid','$gender','$firstname','$lastname','$email','$phone','$address','$city','$monthlybill','$contractload','$roofarea','$phase')";
+	}
 
+	else
+	{
+		if(empty($_POST['firstname']))
+			{
+				echo "First Name Cannot Be Empty<br>";
+			}
 
-
+		if(empty($_POST['lastname']))
+			{
+				echo "Last Name Cannot Be Empty<br>";
+			}
+		if(empty($_POST['email']))
+			{
+				echo "Mother's Name Cannot Be Empty<br>";
+			}
+		if(empty($_POST['phone']))
+			{
+				echo "Father's Name Cannot Be Empty<br>";
+			}
+		if(empty($_POST['city']))
+			{
+				echo "Gender Cannot Be Empty<br>";
+			}
+		if(empty($_POST['type']))
+			{
+				echo "Date Of Birth Cannot Be Empty<br>";
+			}
+			exit;
+	}
+	
 	if($conn->query($sql))
 	{
 		echo $ssid;
