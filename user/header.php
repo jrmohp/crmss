3fd651edbb68b8907f76e44d01cf6184bcb95c2b
iterjrm <codingjrm@gmail.com>
2017-07-13 14:55:54 +0530

    a

--- user/header.php
+++ user/header.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <base href="http://smartsolar.co.in/jrm/user">
+    <base href="http://smartsolar.co.in/jrm/user/">
     <!-- Bootstrap -->
     <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
