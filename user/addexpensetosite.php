61fe5c41e729755d6331a28dc1989c4399951238
Crypto <kamakhya166@gmail.com>
2017-07-15 14:14:23 +0530

    xoxo

--- user/addexpensetosite.php
+++ user/addexpensetosite.php
@@ -326,14 +326,14 @@ $site=$_POST['siteid'];
 
 
 
-
+      <div class="row">
 
       <?php
 
 require("../php/connect.php");
 
 
-$query ="SELECT property FROM siteprop WHERE siteid=''"; 
+$query ="SELECT property FROM siteprop WHERE siteid='$site'"; 
 
 
 if($result=mysqli_query($conn,$query))
@@ -343,11 +343,11 @@ if($result=mysqli_query($conn,$query))
     
       foreach ($row as $val) 
       { 
-            echo'<div class="col-md-12 col-sm-12 col-xs-12"  >
-                      <label for="$val" class="col-md-4 col-sm-4 col-xs-4"><span class="label label-info">'.$val.'</span></label>
+            echo'<div class="col-md-4 col-sm-4 col-xs-12"  >
+                      <label for="$val" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.'</span></label>
 
                   <input type="text" name="$val" id="$val" class="form-control"';
-                  echo"placeholder='$val' >     
+                  echo"placeholder='$val' readonly="true">     
             </div><br>";
 
       }
@@ -357,7 +357,7 @@ if($result=mysqli_query($conn,$query))
 }
 
 ?>
-
+                  </div>
                     </div>
                     </div>
                     </body>
