91e0a58cf3cfdd43d79c6ed14cc1ee848fa67b57
Geetswain <geetsswain@gmail.com>
2017-07-15 16:58:29 +0530

    check

--- user/viewuser.php
+++ user/viewuser.php
@@ -46,6 +46,10 @@ if($result=mysqli_query($conn,$query))
 {
    while ($row=mysqli_fetch_row($result))
     {
+    	echo "<pre>";
+    	echo $udata;
+    	echo "</pre>";
+    	exit();
 		foreach ($row as $udata) 
 		{ 
 		  echo "<tr align='center'>";
