90ebdfc9c8c773273f25b96d60d357280425bcbf
iterjrm <codingjrm@gmail.com>
2017-07-15 14:06:37 +0530

    a

--- user/addexpensetosite.php
+++ user/addexpensetosite.php
@@ -328,35 +328,7 @@ $site=$_POST['siteid'];
 
 
 
-      <?php
-
-require("../php/connect.php");
-
-
-$query ="SELECT property FROM siteprop WHERE siteid=$site"; 
-
-
-if($result=mysqli_query($conn,$query))
-{
-   while ($row=mysqli_fetch_row($result))
-    {
-    
-      foreach ($row as $val) 
-      { 
-            echo'<div class="col-md-12 col-sm-12 col-xs-12"  >
-                      <label for="$val" class="col-md-4 col-sm-4 col-xs-4"><span class="label label-info">'.$val.'</span></label>
-
-                  <input type="text" name="$val" id="$val" class="form-control"';
-                  echo"placeholder='$val' >     
-            </div><br>";
-
-      }
       
-    }
-
-}
-
-?>
 
                     </div>
                     </div>
