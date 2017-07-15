c29782df4a9008f612819b3a2f4cdaba03cfe8c6
iterjrm <codingjrm@gmail.com>
2017-07-15 12:52:39 +0530

    a

--- user/addsite.php
+++ user/addsite.php
@@ -58,27 +58,9 @@
 	    			</div>
 
 
-	    			<div class="col-md-6 col-sm-6 col-xs-12" >
-                     	<label for="mname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Expense Count</span></label>
+	    			
 
-                   		  	<input type="text" name="ecount" id="ecount" class="form-control" placeholder="Expense Count">
-       		  		</div>
-
-			</div>
-
-
-			<br>
-
-			<div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                    <button  class="btn btn-success col-md-12 "  align='center' id="next" onclick="regc()">Next</button>
-                    </div>
-                    
-          	</div>
-
-            <div id="ddata">
-              
-            </div>
+           
 
             <div class="row">
                     <div class="col-md-12 col-sm-12 col-xs-12">
@@ -127,72 +109,10 @@ function addsite()
     
     var proparr= new Array();
     var sitename=$('#sitename').val();
+    var siteid=$('#siteid').val();
     var supervisor=$('#supervisor').val();
     
-    while(c2<=ec)
-    { 
-      
-        proparr.push("#prop"+c2);
-        
-       
-        c2++;
-       
-    }
 
-    var propid=new Array();
-    
-    c2=0;
-    while(c2<ec)
-    {
-         
-        propid.push(proparr[c2]);
-       
-       
-        if(c2==ec)
-        {
-          break;
-        }
-        c2++;
-        
-    }
-
-    
-
-
-
-
-var insertcount=0;
-    c2=0;
-
-    while(c2<ec)
-      
-
-    {
-
-        
-        var siteid=$('#siteid').val();
-         
-        var propfinal=$(propid[c2]).val();
-        
-        var data={'siteid':siteid,'prop':propfinal};
-        
-        
-
-
-
-        $.post('../php/addsitecontroller.php',data,function(info){
-            
-            
-
-        });
-
-     c2++;
-       
-       
-    }
-
-    alert("Properties Added To Site");
-    
 
     
    
