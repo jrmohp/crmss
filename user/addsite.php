8173f5cdc92e882bb9edf2b660a955ce93bef270
iterjrm <codingjrm@gmail.com>
2017-07-15 12:40:16 +0530

    a

--- user/addsite.php
+++ user/addsite.php
@@ -34,7 +34,7 @@
 	  				<div class="col-md-6 col-sm-6 col-xs-12"  >
               		  	<label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Site Id</span></label>
 
-            		    <select class="form-control">
+            		    <select class="form-control" id="siteid">
                     
 
                       <?php 
