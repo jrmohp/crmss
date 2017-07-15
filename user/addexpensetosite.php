1e0732439f904467369294d29fa9639ff8863c71
iterjrm <codingjrm@gmail.com>
2017-07-15 14:01:37 +0530

    a

--- user/addexpensetosite.php
+++ user/addexpensetosite.php
@@ -29,7 +29,7 @@
           <form action="" method="post">
                       <label for="advt" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">Site</span></label>
 
-                         <?php echo"<input type='text'  readonly='true'  value=$_POST['siteid']>" ?>
+                       
                 </div>
 
             
