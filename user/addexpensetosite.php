a6951acdf16727770f63737abd6a0e6cc5ea3d6c
Crypto <kamakhya166@gmail.com>
2017-07-15 14:59:33 +0530

    sad

--- user/addexpensetosite.php
+++ user/addexpensetosite.php
@@ -345,29 +345,28 @@ if($result=mysqli_query($conn,$query))
     {
     
       foreach ($row as $val) 
-      {       
-          $quantid=$val+"quant";
-          $rateid=$val+"rate";
-          $totalid=$val+"total"; 
-
+      { 
             echo'<div class="row">
                   <div class="col-md-4 col-sm-4 col-xs-12"  >
-                      <label for="$quantid" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.' Quantity'.'</span></label>
+                      <label for="$val" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.' Quantity'.'</span></label>
 
-                  <input type="text" name="$quantid" id="$quantid" class="form-control"placeholder='Quantity'  onchange="total('postcharge')">     
-            </div><br>
+                  <input type="text" name="$val" id="$val" class="form-control"';
+                  echo"placeholder='$val' readonly='true' onchange="total('postcharge')">     
+            </div><br>";
 
-            <div class="col-md-4 col-sm-4 col-xs-12"  >
-                      <label for="$rateid" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.' Rate'.'</span></label>
+            echo'<div class="col-md-4 col-sm-4 col-xs-12"  >
+                      <label for="$val" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.' Rate'.'</span></label>
 
-                  <input type="text" name="$rateid" id="$rateid" class="form-control" placeholder='Rate'  onchange="total('postcharge')">     
-            </div><br> 
-            <div class="row">
+                  <input type="text" name="$val" id="$val" class="form-control"';
+                  echo"placeholder='$val' readonly='true' onchange="total('postcharge')">     
+            </div><br>";
+            echo'<div class="row">
                   <div class="col-md-4 col-sm-4 col-xs-12"  >
-                      <label for="$totalid" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.' Total'.'</span></label>
+                      <label for="$val" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.' Total'.'</span></label>
 
-                  <input type="text" name="$totalid" id="$totalid" class="form-control" placeholder='Total' readonly='true'>     
-            </div><br>';
+                  <input type="text" name="$val" id="$val" class="form-control"';
+                  echo"placeholder='$val' readonly='true' >     
+            </div><br>";
 
 
       }
