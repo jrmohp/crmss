0697b81bd12dfebde3bc2331e3ef763675db8f69
Crypto <kamakhya166@gmail.com>
2017-07-16 21:52:46 +0530

    g

--- user/editemployee.php
+++ user/editemployee.php
@@ -296,9 +296,13 @@
                    </div>
                   <br>
                   <div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
+                   <div class="col-md-6 col-sm-6 col-xs-12">
+                    <input type="hidden" name="editid" id="editid" class="form-control" value="<?php echo $editid?>">     
+                         </div>
+                    <div class="col-md-6 col-sm-6 col-xs-12">
                     <button  class="btn btn-success col-md-12 " type="submit" align='center' id="sendmail">Send</button>
                     </div>
+                    
                   </div>
 
 
