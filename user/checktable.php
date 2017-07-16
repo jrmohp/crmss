2b4c27f7b038b088373b80fd116dd8fc15c2d6e5
iterjrm <codingjrm@gmail.com>
2017-07-16 12:04:46 +0530

    a

--- user/checktable.php
+++ user/checktable.php
@@ -36,12 +36,9 @@
 
 
 
-        <div class="col-md-3 left_col">
-          <div class="left_col scroll-view">
-            <div class="navbar nav_title" style="border: 0;">
-             
+       
 
-            <div class="clearfix"></div>
+           
 
             <!-- menu profile quick info -->
            
