63785d2f16b15a583dd0141cc2786a3b69b514eb
iterjrm <codingjrm@gmail.com>
2017-07-16 12:00:53 +0530

    a

--- user/checktable.php
+++ user/checktable.php
@@ -28,27 +28,18 @@
     <link href="../build/css/custom.min.css" rel="stylesheet">
   </head>
 
-  <body class="nav-md">
-    <div class="container body">
-      <div class="main_container">
-        <div class="col-md-3 left_col">
-          <div class="left_col scroll-view">
-            <div class="navbar nav_title" style="border: 0;">
-              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
-            </div>
+ 
+<?php
 
-            <div class="clearfix"></div>
+
+  include "header.php";
+?>
+
+
+
 
             <!-- menu profile quick info -->
-            <div class="profile clearfix">
-              <div class="profile_pic">
-                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
-              </div>
-              <div class="profile_info">
-                <span>Welcome,</span>
-                <h2>John Doe</h2>
-              </div>
-            </div>
+           
             <!-- /menu profile quick info -->
 
             <br />
