fbe73ddc418d777abea9baf00f6e133aa7ab1f48
iterjrm <codingjrm@gmail.com>
2017-07-16 12:31:33 +0530

    a

--- user/checkoutput.php
+++ user/checkoutput.php
@@ -10,23 +10,7 @@ if($result=mysqli_query($conn,$query))
     {	
 		foreach ($row as $val) 
 		{ 
-		  echo "<tr align='center'>";
-	      echo "<td>".$val['username']."</td>";
-	      echo "<td>".$val['type']."</td>";
-	      echo "<td>".$val['firstname']."</td>";
-	      echo "<td>".$val['lastname']."</td>";
-	      echo "<td>".$val['email']."</td>";
-	      echo "<td>".$val['mobile']."</td>";
-	      echo "<td>".$val['peraddress']."</td>";
-	      echo "<td>".$val['city']."</td>";
-		  echo "<td>".$val['mbill']."</td>";
-	      echo "<td>".$val['cload']."</td>";
-	      echo "<td>".$val['rarea']."</td>";
-	      echo "<td>".$val['phase']."</td>";
-	      echo "<td>".$val['referredby']."</td>";
-	      echo "<td>".$val['remarks']."</td>";
-	      echo "<td>".$val['req']."</td>";
-	      echo "</tr>";
+		  echo $val;
 
     	}   
     }
