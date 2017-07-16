cbc9a66034486408e9defa5758e7c803e62d031c
iterjrm <codingjrm@gmail.com>
2017-07-16 13:14:16 +0530

    a

--- user/checktable.php
+++ user/checktable.php
@@ -358,12 +358,7 @@
                                 <td>2011/01/25</td>
                                 <td>$112,000</td>
                                 <td>$112,000</td>
-                                 <td>Donna Snider</td>
-                                <td>Customer Support</td>
-                                <td>New York</td>
-                                <td>27</td>
-                                <td>2011/01/25</td>
-                                <td>$112,000</td>
+                                 
                               </tr>
                             </tbody>
                           </table>
