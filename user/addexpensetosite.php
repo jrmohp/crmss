72cf4285a1f252c9743438534dd4c9beb0211458
iterjrm <codingjrm@gmail.com>
2017-07-16 17:27:06 +0530

    a

--- user/addexpensetosite.php
+++ user/addexpensetosite.php
@@ -350,6 +350,7 @@ if($result=mysqli_query($conn,$query))
           $quantid=$val."quant";
           $rateid=$val."rate";
           $totalid=$val."total";
+          $oc="'".$val."'";
 
             echo "<div class='row'> <div class='ln_solid'></div><div class='col-md-4 col-sm-4 col-xs-12'  ><label for='$quantid' class='col-md-1 col-sm-1 col-xs-1'><span class='label label-info'>'.$val.' Quantity'.'</span></label><input type='text' name='$quantid' id='$quantid' class='form-control'";
                   echo"placeholder='Quantity' onchange='total($val)'  > </div>";
@@ -357,7 +358,7 @@ if($result=mysqli_query($conn,$query))
                       <label for="$rateid" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.' Rate'.'</span></label>';
 
                   echo "<input type='text' name='$rateid' id='$rateid' class='form-control'";
-                  echo"placeholder='Rate' onchange='total('$val')'>     
+                  echo"placeholder='Rate' onchange='total($oc)'>     
             </div>";
             echo'<div class="col-md-4 col-sm-4 col-xs-12"  >
                       <label for="$totalid" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.' Total'.'</span></label>
