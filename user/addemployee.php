88e9c3a9c9b47d2593f39c40dc79c18b91a935aa
Crypto <kamakhya166@gmail.com>
2017-07-17 16:17:31 +0530

    asd

--- user/addemployee.php
+++ user/addemployee.php
@@ -386,6 +386,18 @@
 
     }
 </script>
+<script type="text/javascript">
+$(function () {
+  $('#demo-form').parsley().on('field:validated', function() {
+    var ok = $('.parsley-error').length === 0;
+    $('.bs-callout-info').toggleClass('hidden', !ok);
+    $('.bs-callout-warning').toggleClass('hidden', ok);
+  })
+  .on('form:submit', function() {
+    return false; // Don't submit form for this demo
+  });
+});
+</script>
 <?php
 
 
