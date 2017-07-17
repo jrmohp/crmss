4883b8350475a4c4ae30a14ff5cb0af167458160
Crypto <kamakhya166@gmail.com>
2017-07-17 22:27:40 +0530

    asd

--- user/addfranchise.php
+++ user/addfranchise.php
@@ -135,12 +135,32 @@
 
 
                      <div class="row">
-                     		<div class="col-md-9 col-sm-9 col-xs-12">
+                     		<div class="col-md-12 col-sm-12 col-xs-12">
       		              	 <label for="pa" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Address</span></label>
             		          <textarea class="pa form-control" placeholder="Address" rows="5" name="address"></textarea>
                  			</div>
                       </div>
                       <br>
+
+                      <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xs-12" >
+                        <label for="username" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Username</span></label>
+
+                        <input type="text" name="username" id="username" class="form-control" placeholder="Username">
+                        </div>
+                        <div class="col-md-12 col-sm-12 col-xs-12" >
+                        <label for="accountid" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Account ID</span></label>
+
+                        <input type="text" name="accountid" id="accountid" class="form-control" placeholder="Account ID">
+                        </div>
+                        <div class="col-md-12 col-sm-12 col-xs-12" >
+                        <label for="password" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label     label-info">Password</span></label>
+
+                        <input type="text" name="password" id="password" class="form-control" placeholder="Password">
+                        </div>
+
+                      <br>
+
                       <div class="row">
                     <div class="col-md-12 col-sm-12 col-xs-12">
                     <button  class="btn btn-success col-md-12 " type="submit" align='center' id="sendmail">Send</button>
