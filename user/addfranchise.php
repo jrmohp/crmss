ad2f5ef713358fba35cd01215dfb1ec2294e1940
Crypto <kamakhya166@gmail.com>
2017-07-17 16:31:00 +0530

    ads

--- user/addfranchise.php
+++ user/addfranchise.php
@@ -135,10 +135,11 @@
 
 
                      <div class="row">
-                     		</div class="col-md-9 col-sm-9 col-xs-12">
+                     		<div class="col-md-9 col-sm-9 col-xs-12">
       		              	 <label for="pa" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Address</span></label>
             		          <textarea class="pa form-control" placeholder="Address" rows="5" name="adsress"></textarea>
                  			</div>
+                      </div>
                       <br>
                       <div class="row">
                     <div class="col-md-12 col-sm-12 col-xs-12">
