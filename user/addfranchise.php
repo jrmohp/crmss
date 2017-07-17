c86f715baf040153719c97f77309f16813e8771b
Geetswain <geetsswain@gmail.com>
2017-07-17 17:15:47 +0530

    check

--- user/addfranchise.php
+++ user/addfranchise.php
@@ -137,7 +137,7 @@
                      <div class="row">
                      		<div class="col-md-9 col-sm-9 col-xs-12">
       		              	 <label for="pa" class="col-md-1 col-sm-1 col-xs-1 control-label multiline"><span class="label label-info">Address</span></label>
-            		          <textarea class="pa form-control" placeholder="Address" rows="5" name="adsress"></textarea>
+            		          <textarea class="pa form-control" placeholder="Address" rows="5" name="address"></textarea>
                  			</div>
                       </div>
                       <br>
