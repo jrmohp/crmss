26880b09c9d4444c21aafb48ab0daba50a68d822
Crypto <kamakhya166@gmail.com>
2017-07-18 13:00:26 +0530

    sad

--- php/employeeeditcontroller.php
+++ php/employeeeditcontroller.php
@@ -4,6 +4,9 @@ require("connect.php");
 if(!empty($_POST['fname']) && !empty($_POST['lname'])&& !empty($_POST['fn']) && !empty($_POST['mn']) && !empty($_POST['gender'])&& !empty($_POST['dob'])&& !empty($_POST['bg'])&& !empty($_POST['pa'])&& !empty($_POST['ta'])&& !empty($_POST['acard'])&& !empty($_POST['accno'])&& !empty($_POST['ifsc'])&& !empty($_POST['cno'])&& !empty($_POST['email'])&& !empty($_POST['wphno'])&& !empty($_POST['wemail'])&& !empty($_POST['username'])&& !empty($_POST['password']) && !empty($_POST['empid']))	
 	{
 		
+	$empid=mysqli_real_escape_string($conn, $_POST['empid']);
+	$username=mysqli_real_escape_string($conn, $_POST['username']);
+	$password=mysqli_real_escape_string($conn, $_POST['password']);
 	$fname=mysqli_real_escape_string($conn, $_POST['fname']);
 	$mname=mysqli_real_escape_string($conn, $_POST['mname']);
 	$lname=mysqli_real_escape_string($conn, $_POST['lname']);
@@ -27,12 +30,10 @@ if(!empty($_POST['fname']) && !empty($_POST['lname'])&& !empty($_POST['fn']) && 
 	$altemail=mysqli_real_escape_string($conn, $_POST['altemail']);
 	$wphno=mysqli_real_escape_string($conn, $_POST['wphno']);
 	$wemail=mysqli_real_escape_string($conn, $_POST['wemail']);
-	$empid=mysqli_real_escape_string($conn, $_POST['empid']);
-	$username=mysqli_real_escape_string($conn, $_POST['username']);
-	$password=mysqli_real_escape_string($conn, $_POST['password']);
+	
 	$editid=mysqli_real_escape_string($conn, $_POST['editid']);
 	
-$sql="UPDATE addemployee SET empid='$empid',username='$username',pass='$password',fname='$fname',mname='$mname',lname='$lname',fn='$fn',mn='$mn',gender='$gender',dob='$dob',bg='$bg',im='$im',pa='$pa',ta='$ta',acard='$acard',pcard='$pcard',epf='$epf',exp='$exp',accno='$accno',ifsc='$ifsc',cno='$cno',altcno='$altcno',email='$email',altemail='$altemail',wphone='$wphone',wemail='$wemail' WHERE id=$editid ";
+$sql = "UPDATE addemployee SET empid='$empid',username='$username',pass='$password',fname='$fname',mname='$mname',lname='$lname',fn='$fn',mn='$mn',gender='$gender',dob='$dob',bg='$bg',im='$im',pa='$pa',ta='$ta',acard='$acard',pcard='$pcard',epf='$epf',exp='$exp',accno='$accno',ifsc='$ifsc',cno='$cno',altcno='$altcno',email='$email',altemail='$altemail',wphone='$wphone',wemail='$wemail' WHERE id=$editid ";
 	
 	if($conn->query($sql))
 	{
