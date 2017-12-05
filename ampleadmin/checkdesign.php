fa40a6394e4374bc1a8e37ad06b0e89f4567775e
Sidharth Samantaray <sidharthsray@gmail.com>
2017-12-05 18:10:37 +0530

    ss

--- ampleadmin/checkdesign.php
+++ ampleadmin/checkdesign.php
@@ -8,12 +8,8 @@
 
 
  
-<!DOCTYPE html>
-<html>
-<head>
+
   <title>Add Faculty | Mega ATV Championship  </title>
-</head>
-<body>
 
  	<div class="row" id="body2">
             <div class="col-md-12 col-sm-12 col-xs-12">
