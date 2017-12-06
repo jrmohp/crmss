1dcd33ecfd0cbc26c40ba548a57a20c16c06165f
vishal-98 <vishalswain98@gmail.com>
2017-12-06 13:51:34 +0530

    aa

--- ampleadmin/header.php
+++ ampleadmin/header.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" type="image/png" sizes="16x16" href="../plugins/images/favicon.png">
-    <title>Ample Admin Template - The Ultimate Multipurpose admin template</title>
+    
     <!-- Bootstrap Core CSS -->
     <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Menu CSS -->
