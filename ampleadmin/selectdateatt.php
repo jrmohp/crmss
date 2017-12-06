feb168057262a852af5b8fe9e955a2866491e9eb
vishal-98 <vishalswain98@gmail.com>
2017-12-06 15:12:47 +0530

    a

--- ampleadmin/selectdateatt.php
+++ ampleadmin/selectdateatt.php
@@ -18,6 +18,27 @@
 <div class="col-md-4 col-sm-12">
                         
                                     <form>
+                                    	<div class="bootstrap-iso">
+ <div class="container-fluid">
+  <div class="row">
+   <div class="col-md-6 col-sm-6 col-xs-12">
+
+    <!-- Form code begins -->
+    <form method="post">
+      <div class="form-group"> <!-- Date input -->
+        <label class="control-label" for="date">Date</label>
+        <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text"/>
+      </div>
+      <div class="form-group"> <!-- Submit button -->
+        <button class="btn btn-primary " name="submit" type="submit">Submit</button>
+      </div>
+     </form>
+     <!-- Form code ends --> 
+
+    </div>
+  </div>    
+ </div>
+</div>
                                         
                                         <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
                                         
