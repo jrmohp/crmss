b782bdad83b0b92e88f7dab8e2afd66bf575ddf5
vishal-98 <vishalswain98@gmail.com>
2017-12-06 15:07:08 +0530

    aa

--- ampleadmin/selectdateatt.php
+++ ampleadmin/selectdateatt.php
@@ -11,60 +11,19 @@
 
 
 
-	<form>
+	
 		<div class="row ">
 
 			<div class="col-md-4"></div>
 <div class="col-md-4 col-sm-12">
-                        <div class="white-box">
-                            <h3 class="box-title m-b-0">Sample Forms with icon</h3>
-                            <p class="text-muted m-b-30 font-13"> Bootstrap Elements </p>
-                            <div class="row">
-                                <div class="col-sm-12 col-xs-12">
+                        
                                     <form>
-                                        <div class="form-group">
-                                            <label for="exampleInputuname">User Name</label>
-                                            <div class="input-group">
-                                                <div class="input-group-addon"><i class="ti-user"></i></div>
-                                                <input type="text" class="form-control" id="exampleInputuname" placeholder="Username"> </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputEmail1">Email address</label>
-                                            <div class="input-group">
-                                                <div class="input-group-addon"><i class="ti-email"></i></div>
-                                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email"> </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputphone">Phone</label>
-                                            <div class="input-group">
-                                                <div class="input-group-addon"><i class="ti-mobile"></i></div>
-                                                <input type="tel" class="form-control" id="exampleInputphone" placeholder="Enter phone"> </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputpwd1">Password</label>
-                                            <div class="input-group">
-                                                <div class="input-group-addon"><i class="ti-lock"></i></div>
-                                                <input type="password" class="form-control" id="exampleInputpwd1" placeholder="Enter email"> </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputpwd2">Confirm Password</label>
-                                            <div class="input-group">
-                                                <div class="input-group-addon"><i class="ti-lock"></i></div>
-                                                <input type="password" class="form-control" id="exampleInputpwd2" placeholder="Enter email"> </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="checkbox checkbox-success">
-                                                <input id="checkbox1" type="checkbox">
-                                                <label for="checkbox1"> Remember me </label>
-                                            </div>
-                                        </div>
+                                        
                                         <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
-                                        <button type="submit" class="btn btn-inverse waves-effect waves-light">Cancel</button>
+                                        
                                     </form>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
+                         
 			<div class="col-md-4"></div>
 
 
