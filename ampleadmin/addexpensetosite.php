0e2567e2308995d63d60027bede394aa7714e2e3
crypto166 <kamakhya166@gmail.com>
2017-12-07 17:30:06 +0530

    as

--- ampleadmin/addexpensetosite.php
+++ ampleadmin/addexpensetosite.php
@@ -352,12 +352,12 @@ if($result=mysqli_query($conn,$query))
             echo "<div class='row'> <div class='ln_solid'></div><div class='col-md-4 col-sm-4 col-xs-12'  ><label for='$val' class='col-md-1 col-sm-1 col-xs-1'><span class='label label-info'>$val Quantity</span></label><input type='text'  name='$quantid' id='$quantid' class='form-control qtys'";
                   echo 'placeholder="Quantity" onchange="total('.$oc.')"> </div>';
             echo'<div class="col-md-4 col-sm-4 col-xs-12"  >
-                      <label for="$val" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.'$val'.' Rate'.'</span></label>';
+                      <label for="$val" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.' Rate'.'</span></label>';
 
                   echo "<input type='text' name='$rateid' id='$rateid' class='form-control'";
                   echo 'placeholder="Rate" onchange="total('.$oc.')"> </div>';
             echo'<div class="col-md-4 col-sm-4 col-xs-12"  >
-                      <label for="$val" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.'$val'.' Total'.'</span></label>';
+                      <label for="$val" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">'.$val.' Total'.'</span></label>';
 
                   echo "<input type='text' class='tots form-control' name='$totalid' id='$totalid' ";
                   echo"placeholder='Total' readonly='true'>     
