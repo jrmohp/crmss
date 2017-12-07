89e77ab874fabd6c28a9ce12db44ac6b9ca64edf
crypto166 <kamakhya166@gmail.com>
2017-12-07 14:46:45 +0530

    sda

---
+++ php/addexpensetositecontroller.php
@@ -0,0 +1,51 @@
+<?php
+
+require("connect.php");
+
+if(!empty($_POST['fuelquant']) && !empty($_POST['fuelrate'])&& !empty($_POST['fueltotal']))	
+	{
+
+	}
+
+if(!empty($_POST['ecquant']) && !empty($_POST['ecrate'])&& !empty($_POST['ectotal']))	
+	{
+		
+	}
+
+if(!empty($_POST['transquant']) && !empty($_POST['transrate'])&& !empty($_POST['transtotal']))	
+	{
+		
+	}
+
+if(!empty($_POST['advtquant']) && !empty($_POST['advtrate'])&& !empty($_POST['advttotal']))	
+	{
+		
+	}
+
+if(!empty($_POST['foodquant']) && !empty($_POST['foodrate'])&& !empty($_POST['foodtotal']))	
+	{
+		
+	}
+
+if(!empty($_POST['rentquant']) && !empty($_POST['rentrate'])&& !empty($_POST['renttotal']))	
+	{
+		
+	}
+
+if(!empty($_POST['telebillquant']) && !empty($_POST['telebillrate'])&& !empty($_POST['telebilltotal']))	
+	{
+		
+	}
+
+if(!empty($_POST['elecbillquant']) && !empty($_POST['elecbillrate'])&& !empty($_POST['elecbilltotal']))	
+	{
+		
+	}
+
+if(!empty($_POST['postchargequant']) && !empty($_POST['postchargerate'])&& !empty($_POST['postchargetotal']))	
+	{
+		
+	}
+
+
+?>
