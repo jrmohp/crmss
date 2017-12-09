9800b7628a05f805344b42655e7050091cd66847
amandas123 <amantheryder23@gmail.com>
2017-12-09 16:54:29 +0530

    sasa

--- ampleadmin/viewcustomer.php
+++ ampleadmin/viewcustomer.php
@@ -13,7 +13,7 @@
 
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="panel panel-default">
-                            <div class="panel-heading">customer details</div>
+                            <div class="panel-heading"><h3>customer details</h3></div>
                             <div class="panel-wrapper collapse in">
                                 <div class="panel-body">
                                      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
