cab69a3eab08ab064dc5c971d9a33f124ac9856d
vishal-98 <vishalswain98@gmail.com>
2017-12-09 15:21:34 +0530

    ghfd

--- ampleadmin/addcustomer.php
+++ ampleadmin/addcustomer.php
@@ -11,7 +11,7 @@
             <div class="row">
 <center>
               <div class="col-md-11 col-xs-12 " style="float: none; margin: 0 auto;">
-                <div class="x_panel">
+                <div class="x_panel"> 
                   <div class="x_title">
                      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="panel panel-default">
