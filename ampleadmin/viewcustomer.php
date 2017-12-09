8a733f0fb99c55f54d02f8756e59367c33490439
amandas123 <amantheryder23@gmail.com>
2017-12-09 16:48:39 +0530

    ac

--- ampleadmin/viewcustomer.php
+++ ampleadmin/viewcustomer.php
@@ -26,7 +26,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Customer Database<small>Please Wait Until The Page Fully Loads</small></h2>
+                
                     <ul class="nav navbar-right panel_toolbox">
                       <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                       </li>
@@ -130,7 +130,7 @@
                   </div>
                 </div>
               </div>
-              
+
                                 </div>
                             </div>
                         </div>
