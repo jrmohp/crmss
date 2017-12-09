355df3839961c4d0de5fc2b5e3c39fe97409ca94
amandas123 <amantheryder23@gmail.com>
2017-12-09 16:52:06 +0530

    ;lkjhgfds

--- ampleadmin/viewcustomer.php
+++ ampleadmin/viewcustomer.php
@@ -25,32 +25,12 @@
             
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
-                  <div class="x_title">
-                
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
+                  
                   <div class="x_content">
                     <div class="row">
                       <div class="col-sm-12">
                         <div class="card-box table-responsive">
-                          <p class="text-muted font-13 m-b-30">
-                           Empoloyee has no rights to copy or use the user data of the Smart Solar without the consent of the Company Officials.
-                          </p>
+                          
 
                           <table id="datatable-keytable" class="table table-striped table-bordered">
                             <thead>
