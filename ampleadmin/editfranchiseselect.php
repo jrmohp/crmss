28215ef511946b94940016ba2fdc2627f4e36611
amandas123 <amantheryder23@gmail.com>
2017-12-10 13:22:36 +0530

    fg

--- ampleadmin/editfranchiseselect.php
+++ ampleadmin/editfranchiseselect.php
@@ -13,6 +13,17 @@
 
   <title>Edit Franchise | Smart Solar  </title>
 
+ <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Customer database</div>
+                            <div class="panel-wrapper collapse in">
+                                <div class="panel-body">
+                                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading"></div>
+                            <div class="panel-wrapper collapse in">
+                                <div class="panel-body">
+                                    
 
 
 
@@ -106,6 +117,18 @@
                   </div>
                 </div>
               </div>
+              
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+  
 
             
 
