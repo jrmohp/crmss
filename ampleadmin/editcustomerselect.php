67fef98c0e0205a8546087f1a3aacaa6dcbe29dd
amandas123 <amantheryder23@gmail.com>
2017-12-10 12:18:02 +0530

    sd

--- ampleadmin/editcustomerselect.php
+++ ampleadmin/editcustomerselect.php
@@ -9,7 +9,7 @@ include "header.php";
 
    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="panel panel-default">
-                            <div class="panel-heading"></div>
+                            <div class="panel-heading">Customer database</div>
                             <div class="panel-wrapper collapse in">
                                 <div class="panel-body">
                                      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -51,7 +51,7 @@ include "header.php";
     <div class="col-sm-12">
         <div class="white-box">
             <h3 class="box-title m-b-0">Data Table</h3>
-            <p class="text-muted m-b-30">Data table example</p>
+            <p class="text-muted m-b-30">  Employee has no rights to copy or use the user data of the Smart Solar without the consent of the Company Officials.</p>
             <div class="table-responsive">
                 <table id="myTable" class="table table-striped">
                     <thead>
