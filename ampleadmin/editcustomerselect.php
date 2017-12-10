e1c78b45017bdd52ee7dfe04d4feaeed7e4f6362
amandas123 <amantheryder23@gmail.com>
2017-12-10 11:56:18 +0530

    sd

--- ampleadmin/editcustomerselect.php
+++ ampleadmin/editcustomerselect.php
@@ -53,28 +53,12 @@
             
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
-                  <div class="x_title">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div class="panel panel-default">
+                
                             <div class="panel-heading"></div>
                             <div class="panel-wrapper collapse in">
                                 <div class="panel-body">
                                     <h2>Customer Database<small><br>Please Wait Until The Page Fully Loads</small></h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
+                  
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
@@ -144,7 +128,7 @@
                     </div>
                   </div>
                 </div>
-              </div>
+              
                                 </div>
                             </div>
                         </div>
