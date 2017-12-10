6cc9165b22d31eb01b07709b80483c1d13f81ac8
amandas123 <amantheryder23@gmail.com>
2017-12-10 12:37:31 +0530

    ad

--- ampleadmin/editemployeeselect.php
+++ ampleadmin/editemployeeselect.php
@@ -1,118 +1,206 @@
+<?php
 
 
- <?php
 
 
-  include "header.php";
-
+include "header.php";
 
 ?>
 
-
- 
-
-  <title>View Customers | Smart Solar  </title>
-
- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Customer database<h2><small>please wait untill the page fully loads</small>></h2></div>
+                            <div class="panel-wrapper collapse in">
+                                <div class="panel-body">
+                                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="panel panel-default">
                             <div class="panel-heading"></div>
                             <div class="panel-wrapper collapse in">
                                 <div class="panel-body">
                                      <div class="row" id="body2">
-            
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Customer Database<small>Please Wait Until The Page Fully Loads</small></h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    <div class="row">
-                      <div class="col-sm-12">
-                        <div class="card-box table-responsive">
-                          <p class="text-muted font-13 m-b-30">
-                           Empoloyee has no rights to copy or use the user data of the Smart Solar without the consent of the Company Officials.
-                          </p>
-
-                          <table id="datatable-keytable" class="table table-striped table-bordered">
-                            <thead>
-                           
-                              <tr align='center'>
-                                 <th>Employee ID</th>
-                                <th>Name</th>
-                              </tr>
-                             
-                            </thead>
 
 
-                            <tbody>
-                              
-                            
+
+<!-- Bootstrap Core CSS -->
+<link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="../plugins/bower_components/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+<link href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
+<!-- Menu CSS -->
+<link href="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
+<!-- animation CSS -->
+<link href="css/animate.css" rel="stylesheet">
+<!-- Custom CSS -->
+<link href="css/style.css" rel="stylesheet">
+<!-- color CSS -->
+<link href="css/colors/blue-dark.css" id="theme" rel="stylesheet">
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
 
 
-                                <?php
 
-                                    require("../php/connect.php");
 
-                                        $query = "SELECT id,empid,fname,lname FROM addemployee"; 
+</div>
 
-                                          if($result=$conn->query($query))
-                                              {
-                                                    while ($row=$result->fetch_array())
-                                                  { 
-                                                            $reqid=$row['id'];
-                                                             echo "<tr align='center'>";
+
+<!-- /row -->
+<div class="row">
+    <div class="col-sm-12">
+        <div class="white-box">
+            <h3 class="box-title m-b-0">Data Table</h3>
+            <p class="text-muted m-b-30">  Employee has no rights to copy or use the user data of the Smart Solar without the consent of the Company Officials.</p>
+            <div class="table-responsive">
+                <table id="myTable" class="table table-striped">
+                    <thead>
+
+                    <tr align='center'>
+                        <th align='center'>Employee ID</th>
+                        <th align='center'>Name</th>
+                       
+
+                    </tr>
+
+                    </thead>
+                    <tbody>
+
+
+
+
+                    <?php
+
+                    require("../php/connect.php");
+
+                    $query = "SELECT * FROM user";
+
+                    if($result=$conn->query($query))
+                    {
+                        while ($row=$result->fetch_array())
+                        {
+
+                             $reqid=$row['id'];
+                                                       echo "<tr align='center'>";
         echo "<td>".$row['empid']."</td>";
         echo "<td>".$row['fname']." ".$row['lname']."</td>";
        
         echo "<td id='$reqid'><button class='btn btn-info editdata' id='$reqid' onclick='DB(this.id)'>Edit</button></td>";
        
         echo "</tr>";
+                        }
+                    }
+                    ?>
 
-                                                    }
-                                                            }
-                                        ?>
-                          
-                                
 
-                            
-                            </tbody>
-                          </table>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+
+
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
+
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+  
  
 
-            
-
-            
-        
 
 
+<!-- /.container-fluid -->
+<footer class="footer text-center"> &copy Smart Solar</footer>
 
-    <script type="text/javascript">
+<!-- /#page-wrapper -->
+
+<!-- /#wrapper -->
+<!-- jQuery -->
+<script src="../plugins/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Menu Plugin JavaScript -->
+<script src="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+<!--slimscroll JavaScript -->
+<script src="js/jquery.slimscroll.js"></script>
+<!--Wave Effects -->
+<script src="js/waves.js"></script>
+<!-- Custom Theme JavaScript -->
+<script src="js/custom.min.js"></script>
+<script src="../plugins/bower_components/datatables/jquery.dataTables.min.js"></script>
+<!-- start - This is for export functionality only -->
+<script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
+<!-- end - This is for export functionality only -->
+<script>
+    $(document).ready(function() {
+        $('#myTable').DataTable();
+        $(document).ready(function() {
+            var table = $('#example').DataTable({
+                "columnDefs": [{
+                    "visible": false,
+                    "targets": 2
+                }],
+                "order": [
+                    [2, 'asc']
+                ],
+                "displayLength": 25,
+                "drawCallback": function(settings) {
+                    var api = this.api();
+                    var rows = api.rows({
+                        page: 'current'
+                    }).nodes();
+                    var last = null;
+                    api.column(2, {
+                        page: 'current'
+                    }).data().each(function(group, i) {
+                        if (last !== group) {
+                            $(rows).eq(i).before('<tr class="group"><td colspan="5">' + group + '</td></tr>');
+                            last = group;
+                        }
+                    });
+                }
+            });
+            // Order by the grouping
+            $('#example tbody').on('click', 'tr.group', function() {
+                var currentOrder = table.order()[0];
+                if (currentOrder[0] === 2 && currentOrder[1] === 'asc') {
+                    table.order([2, 'desc']).draw();
+                } else {
+                    table.order([2, 'asc']).draw();
+                }
+            });
+        });
+    });
+    $('#example23').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    });
+</script>
+<!--Style Switcher -->
+<script src="../plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
+
+
+
+
+
+ <script type="text/javascript">
               
 
                 
@@ -121,7 +209,7 @@
                
 
               
-               var link="http://crm.smartsolar.co.in/ampleadmin/editemployee?id="+btnid;
+               var link="http://crm.smartsolar.co.in/ampleadmin/editcustomer?id="+btnid;
 
                window.location.assign(link);
 
@@ -130,8 +218,3 @@
 
 
             </script>
-
-<?php
-
-include "footer.php";
-?>
