73a9eba6469ddfe96517eab37463d78f279ec213
amandas123 <amantheryder23@gmail.com>
2017-12-10 12:45:32 +0530

    gh

--- ampleadmin/editemployeeselect.php
+++ ampleadmin/editemployeeselect.php
@@ -9,7 +9,7 @@ include "header.php";
 
    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="panel panel-default">
-                            <div class="panel-heading">Customer database<h2><small>please wait untill the page fully loads</small>></h2></div>
+                            <div class="panel-heading">Customer database</div>
                             <div class="panel-wrapper collapse in">
                                 <div class="panel-body">
                                      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
