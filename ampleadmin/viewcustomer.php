19904c1227427a1950fe243457ae2a6f846f87d5
Jyoti Ranjan Mohapatra <codingjrm@gmail.com>
2017-12-10 09:54:36 +0530

    a

--- ampleadmin/viewcustomer.php
+++ ampleadmin/viewcustomer.php
@@ -23,14 +23,7 @@
                                 <div class="panel-body">
                                      <div class="row" id="body2">
             
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  
-                  <div class="x_content">
-                    <div class="row">
-                      <div class="col-sm-12">
-                        <div class="card-box table-responsive">
-                          
+              
 
                           <table id="datatable-keytable" class="table table-striped table-bordered">
                             <thead>
