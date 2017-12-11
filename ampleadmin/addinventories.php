495c397a1e157c914a91af7a880cf76370361650
Jyoti Ranjan Mohapatra <codingjrm@gmail.com>
2017-12-11 15:03:29 +0530

    Add Inventories

---
+++ ampleadmin/addinventories.php
@@ -0,0 +1,56 @@
+
+
+<?php
+
+
+
+
+    include "header.php";
+
+?>
+
+
+
+
+
+    <div class="row">
+        <div class="col-md-4 col-lg-4 col-sm-4 col-xs-12">
+    <input type="text" class="form-control " id="count">
+
+        <br>
+    <br>
+    <button class="form-control btn-success" id="nxt" onclick="gen()">Next</button>
+        </div>
+
+    </div>
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+
+    include "footer.php";
+
+
+?>
+
+
+
+
