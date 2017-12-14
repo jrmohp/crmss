3068253d72861a15937a410846660223e575d156
Sidharth Samantaray <sidharthsray@gmail.com>
2017-12-14 09:22:33 +0530

    vsv

--- user/guda.php
+++ user/guda.php
@@ -1,4 +1,3 @@
-
 <?php
 
 
@@ -10,73 +9,62 @@ include "header.php";
 
 
 
-<?php
+
+    <div class="bootstrap-iso">
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-md-4"></div>
+
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><i class="ti-settings"></i> Choose Site Id </div>
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body">
+                            <form method="post" action="invtrans" >
+
+                                <select class="form-control" id="siteid" name="siteid" >
+
+
+                                    <?php
 
 
 
+                                    $siteids="SELECT  siteid FROM siteinfo";
+
+                                    if($result=mysqli_query($conn,$siteids))
+                                    {
+                                        while ($row=mysqli_fetch_row($result))
+                                        {
+
+                                            foreach ($row as $val)
+                                            {
+                                                echo "<option>$val</option>" ;
+
+                                            }
+
+                                        }
+
+                                    }
+
+                                    ?>
 
 
-     $moradarkar="SELECT * from siteinfo";
+                                </select>
 
-     $result=$conn->query($moradarkar);
-     ?>
+                                <br>
 
-<div class="row">
-    <div class="col-sm-12">
-        <div class="white-box">
-            <h3 class="box-title m-b-0">Data Table</h3>
-            <p class="text-muted m-b-30">Data table example</p>
-            <div class="table-responsive">
-                <table id="myTable" class="table table-striped">
-                    <thead>
+                                <div class="form-group"> <!-- Submit button -->
+                                    <center><input type="submit" class="btn btn-primary " name=""></center>
+                            </form>
 
-                    <tr align='center'>
-                        <th align='center'>ID</th>
-                        <th align='center'>Name</th>
-
-
-
-
-                    </tr>
-
-                    </thead>
-                    <tbody>
-
-<?php
-     while ($row=$result->fetch_assoc())
-     {
-         echo "<tr>";
-         echo "<td>";
-         echo $row['siteid'];
-         echo "</td>";
-
-         echo "<td>";
-
-         echo $row['sitename'];
-         echo "</td>";
-         echo "</tr>";
-     }
-
-
-
-
-
-
-?>
-
-
-
-
-
-
-                    </tbody>
-
-                </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-</div>
 
 
 
@@ -84,97 +72,11 @@ include "header.php";
 
 
 
-</div>
-</div>
-</div>
-</div>
 
-</div>
-</div>
-</div>
-</div>
+<?php
 
 
+include "footer.php";
 
 
-
-<!-- /.container-fluid -->
-<footer class="footer text-center"> &copy Smart Solar</footer>
-
-<!-- /#page-wrapper -->
-
-<!-- /#wrapper -->
-<!-- jQuery -->
-<script src="../plugins/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap Core JavaScript -->
-<script src="bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Menu Plugin JavaScript -->
-<script src="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
-<!--slimscroll JavaScript -->
-<script src="js/jquery.slimscroll.js"></script>
-<!--Wave Effects -->
-<script src="js/waves.js"></script>
-<!-- Custom Theme JavaScript -->
-<script src="js/custom.min.js"></script>
-<script src="../plugins/bower_components/datatables/jquery.dataTables.min.js"></script>
-<!-- start - This is for export functionality only -->
-<script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
-<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
-<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
-<!-- end - This is for export functionality only -->
-<script>
-    $(document).ready(function() {
-        $('#myTable').DataTable();
-        $(document).ready(function() {
-            var table = $('#example').DataTable({
-                "columnDefs": [{
-                    "visible": false,
-                    "targets": 2
-                }],
-                "order": [
-                    [2, 'asc']
-                ],
-                "displayLength": 25,
-                "drawCallback": function(settings) {
-                    var api = this.api();
-                    var rows = api.rows({
-                        page: 'current'
-                    }).nodes();
-                    var last = null;
-                    api.column(2, {
-                        page: 'current'
-                    }).data().each(function(group, i) {
-                        if (last !== group) {
-                            $(rows).eq(i).before('<tr class="group"><td colspan="5">' + group + '</td></tr>');
-                            last = group;
-                        }
-                    });
-                }
-            });
-            // Order by the grouping
-            $('#example tbody').on('click', 'tr.group', function() {
-                var currentOrder = table.order()[0];
-                if (currentOrder[0] === 2 && currentOrder[1] === 'asc') {
-                    table.order([2, 'desc']).draw();
-                } else {
-                    table.order([2, 'asc']).draw();
-                }
-            });
-        });
-    });
-    $('#example23').DataTable({
-        dom: 'Bfrtip',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-</script>
-<!--Style Switcher -->
-<script src="../plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
-
-
-
+?>
