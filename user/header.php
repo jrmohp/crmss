6ba52f5900dfbeb58a189a2028e8727edbc95087
crypto166 <kamakhya166@gmail.com>
2017-12-18 17:09:32 +0530

    asd

--- user/header.php
+++ user/header.php
@@ -262,7 +262,7 @@ require("../php/connect.php");
                         <li><a href="assigntofranchise.php"><span class="hide-menu">Assign Project</span></a></li>
                     </ul>
                 </li>
-                <li> <a href="forms.html" class="waves-effect"><i class="mdi mdi-clipboard-text fa-fw"></i> <span class="hide-menu">Attendance Management<span class="fa arrow"></span></span></a>
+                <li> <a href="forms.html" class="waves-effect"><i class="mdi mdi-book-minus fa-fw"></i> <span class="hide-menu">Attendance Management<span class="fa arrow"></span></span></a>
                     <ul class="nav nav-second-level">
                         <li><a href="selectdateatt.php"><span class="hide-menu">Add Attendance</span></a></li>
                         <li><a href="selectdateviewatt.php"><span class="hide-menu">View Attendance</span></a></li>
