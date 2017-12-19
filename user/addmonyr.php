1809ffb66ba6cd17e0ae8dddfdc53afba8c22175
vishal-98 <vishalswain98@gmail.com>
2017-12-19 16:47:15 +0530

    aa

---
+++ user/addmonyr.php
@@ -0,0 +1,64 @@
+<?php
+
+
+
+
+include "header.php";
+
+?>
+    <title></title>
+
+
+
+
+
+
+    <form>
+        <div class="bootstrap-iso">
+            <div class="container-fluid">
+                <div class="row">
+
+                    <div class="col-md-4"></div>
+
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading"><i class="ti-settings"></i> Choose Month</div>
+                            <div class="panel-wrapper collapse in">
+                                <div class="panel-body">
+                                    <div class="row">
+                                    <form>
+
+                                        <h3>Month and Year:</h3>
+                                        <input type="month" name="bdaymonth">
+                                        <br>
+
+                                        <br>
+
+
+                                        <div class="form-group"> <!-- Submit button -->
+                                            <center><button class="btn btn-success " name="submit" type="submit">Submit</button></center>
+                                        </div>
+
+                                    </form>
+                                    </div>
+                                </div>
+                            </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+    </form>
+
+
+<?php
+
+
+include "footer.php";
+
+
+?>
